298c48604fe10e5ae65bac4a8f6c0ca26c383f99
joão oliveira <h2825766@gmail.com>
2026-05-08 18:27:40 +0100

    Add files via upload

---
+++ pj_agenda/src/js/script.js
@@ -0,0 +1,21 @@
+function agendar() {
+    const nome = document.getElementById('nome').value;
+    const data = document.getElementById('data').value;
+    const hora = document.getElementById('hora').value;
+
+    if (!nome || !data || !hora) {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    const lista = document.getElementById('listaagendamento');
+    const item = document.createElement('p');
+
+    item.textContent = `${nome} agendou para ${data} às ${hora}`;
+
+    lista.appendChild(item);
+
+    document.getElementById('nome').value = '';
+    document.getElementById('data').value = '';
+    document.getElementById('hora').value = '';
+}
